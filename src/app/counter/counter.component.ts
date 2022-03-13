4ba84d27f2ce175581d89f9fe2d4ba1e94ceff46
Ektora <zsoltlucski@gmail.com>
2022-03-13 20:15:03 +0100

    Change detection próba

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -12,6 +12,8 @@ export class CounterComponent implements OnInit {
   @Input() counter : { name: string; id: number; } | undefined;
   @Output() countChange = new EventEmitter<number>();
   count : number = 0;
+  changeDetectionCounter = 0;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,6 +22,12 @@ export class CounterComponent implements OnInit {
   incrementCounter() {
     this.count++;
     this.countChange.emit(this.count);
+  }
+
+  get log(){
+    this.changeDetectionCounter++;
+    console.log(this.changeDetectionCounter);
+    return "change detection test";
   }
 
 }
